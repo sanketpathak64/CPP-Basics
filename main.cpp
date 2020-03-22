4e766b4b34b72ec05204bfb71fe6524e4594d259
sanket pathak <sanketpathak64@gmail.com>
2020-03-22 17:54:58 +0530

    project setup completed

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+ifstream f("data.in");
+ofstream g("data.out");
+
+int main()
+{
+    int a,b;
+    f>>a>>b;
+    int c=a+b;
+    g<<c;
+    return 0;
+}
