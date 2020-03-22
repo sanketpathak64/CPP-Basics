aeaa00f1578de823e8043202f70bbe8f4c9361e3
sanket pathak <sanketpathak64@gmail.com>
2020-03-22 18:17:31 +0530

    first code

--- main.cpp
+++ main.cpp
@@ -1,15 +1,23 @@
-#include <iostream>
-#include<bits/stdc++.h>
+# include <bits/stdc++.h>
 using namespace std;
-
-ifstream f("data.in");
-ofstream g("data.out");
-
 int main()
 {
-    int a,b;
-    f>>a>>b;
-    int c=a+b;
-    g<<c;
+    int a=5, b=8, maximum;
+    // maximum from a and b
+    maximum=max(a, b);
+
+    //swapping two values
+    swap(a, b); // a=8, b=5
+
+    //double pow (double base, double exponent);
+
+    int number=2;
+    double cubicRoot;
+
+    cubicRoot=pow((double)(number), 1.0/3); // 1(or 3) is of type int / 1.0(or 3.0) is of type double
+
+    cout<<fixed<<setprecision(10)<<cubicRoot<<"\n";
+    cout<<fixed<<setprecision(3)<<cubicRoot<<"\n";
+
     return 0;
 }
